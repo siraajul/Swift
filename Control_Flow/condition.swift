243022ce6064aa027d056466ac5ff0a7fc555174
siraajul <shirajulislamparvez@gmail.com>
2024-06-15 07:18:10 +0600

    language check with condition

---
+++ Control_Flow/condition.swift
@@ -0,0 +1,26 @@
+// Problem - 01-------
+// let language: String = "Spanish"
+// if language == "English"{
+//     print("The Default Language is Set to English")
+// }else if language == "Spanish"{
+//     print("The Default Language is Set to Spanish")
+// }else{
+//     print("The Default language has not configure yet! ")
+// }
+
+// Problem-02-------
+// let language: String = "English"
+// switch language{
+//     case "English": print("The default language is set to English")
+//     case "Spanish": print("The default language is set to English")
+//     default: print("The Default language is not configure yet!.")
+
+// }
+
+//Problem-03-------
+let language: String = "Hindi"
+switch language{
+    case "English": print("The default language is set to English")
+    case "Spanish": print("The default language is set to English")
+    default: break
+}
