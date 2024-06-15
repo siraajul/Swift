a9ed6992ecb8d855ece1bf81bafb83fd887ea1c7
siraajul <shirajulislamparvez@gmail.com>
2024-06-15 07:23:21 +0600

    Step 04 & Step 05

--- Control_Flow/exercise.swift
+++ Control_Flow/exercise.swift
@@ -3,3 +3,11 @@ var freeApp: Bool = true
 if freeApp == true{
     print("you are using free version of the app. Buy the full version of the app to get to all of its feature")
 }
+let morningTemperature: Int = 70
+let eveningTemperature: Int = 80 
+
+if morningTemperature < eveningTemperature{
+    print("Morning is Cooler")
+}else{
+    print("Evening is Cooler")
+}
