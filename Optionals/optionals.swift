8d7b6bdb2b246fe5d77233235bb8f674f3911c9a
siraajul <shirajulislamparvez@gmail.com>
2024-06-16 00:48:19 +0600

    Operation

---
+++ Optionals/optionals.swift
@@ -0,0 +1,12 @@
+// Declaring Optional
+// var aValue:Int?
+// var bValue:Int?
+
+// print(aValue)
+// print(bValue)
+
+//Declaring Optional
+var aValue:Int?
+var bValue: Int! = aValue
+
+print(bValue)
